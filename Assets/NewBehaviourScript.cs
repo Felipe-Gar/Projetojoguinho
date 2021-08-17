380acdefafc38a5abe2b8d07a9d322678bdae157
FELIPE DA SILVA GARCIA <garcia.f@alunoescolassociais.org.br>
2021-08-17 15:45:02 -0300

    atualizaçoes

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -8,10 +8,11 @@ public class NewBehaviourScript : MonoBehaviour
     public float velocidade;
     public float tempoPulo;
     private float tempoPulado;
+    private Vector3 posicaoInicial;
     // Start is called before the first frame update
     void Start()
     {
-        
+        posicaoInicial = this.transform.position;
     }
 
     // Update is called once per frame
@@ -33,11 +34,22 @@ public class NewBehaviourScript : MonoBehaviour
         if(Input.GetKey(KeyCode.W)&& tempoPulado <= 0)
         {
            Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
-           Vector2 forca = new Vector2 (0f,10f );
+           Vector2 forca = new Vector2 (0f,9f );
            rb.AddForce(forca,ForceMode2D.Impulse);
-           tempoPulado =tempoPulo;
-
+           tempoPulado = tempoPulo;       
         }
-        tempoPulado-=Time.deltaTime;
+         tempoPulado -= Time.deltaTime;
+
+           if(this.transform.position.y < -14f)
+           {
+               transform.position = posicaoInicial;
+           }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "espinhos_raivosos")
+        {
+            this.transform.position = posicaoInicial;
+        }
     }
 }
