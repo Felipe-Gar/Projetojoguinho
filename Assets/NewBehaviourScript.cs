3a769394f71c5d9a8c6c49e015bb2a7eb47fdf77
FELIPE DA SILVA GARCIA <garcia.f@alunoescolassociais.org.br>
2021-08-18 15:34:39 -0300

    a

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -9,10 +9,15 @@ public class NewBehaviourScript : MonoBehaviour
     public float tempoPulo;
     private float tempoPulado;
     private Vector3 posicaoInicial;
-    // Start is called before the first frame update
+    private Animator anim ;
+    private SpriteRenderer sprite;
     void Start()
+    // Start is called before the first frame update
+    
     {
         posicaoInicial = this.transform.position;
+        anim = this.GetComponent<Animator>();
+        sprite = this.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,12 +25,14 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
         {
+            sprite.flipX =false;
             Vector3 pos = transform.position;
             pos.x += velocidade;
            transform.position = pos;
         }
         if(Input.GetKey(KeyCode.A))
         {
+            sprite.flipX = true;
             Vector3 pos = transform.position;
             pos.x -= velocidade;
             transform.position = pos;
@@ -36,7 +43,8 @@ public class NewBehaviourScript : MonoBehaviour
            Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
            Vector2 forca = new Vector2 (0f,9f );
            rb.AddForce(forca,ForceMode2D.Impulse);
-           tempoPulado = tempoPulo;       
+           tempoPulado = tempoPulo;   
+           anim.SetBool("estacorrendo" , true);    
         }
          tempoPulado -= Time.deltaTime;
 
@@ -44,12 +52,24 @@ public class NewBehaviourScript : MonoBehaviour
            {
                transform.position = posicaoInicial;
            }
+           if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+           {
+               anim.SetBool("estacorrendo", true);
+           }
+           else
+           {
+               anim.SetBool("estacorrendo" , false);
+           }
     }
     void OnCollisionEnter2D(Collision2D col)
     {
         if(col.gameObject.tag == "espinhos_raivosos")
         {
             this.transform.position = posicaoInicial;
+        }
+        if(col.gameObject.tag =="chao")
+        {
+            anim.SetBool("estacorrendo",false);
         }
     }
 }
